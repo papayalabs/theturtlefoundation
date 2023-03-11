77e3449ea2dd9b5fc85ecb94a87f4b5234b65cd1
Manuel Fernandez <papayalabs@gmail.com>
2023-03-11 12:14:53 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_094314) do
+ActiveRecord::Schema.define(version: 2021_12_05_134412) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
