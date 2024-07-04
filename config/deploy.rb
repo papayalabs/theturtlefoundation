e5271f81538e02841b5975239cac36c1cdcb3790
papayalabs <papayalabs@gmail.com>
2024-07-04 15:14:48 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.19.0"
 
 set :application, "theturtlefoundation"
-set :repo_url, "git@github.com:papayalabs/theturtlefoundation_new.git"
+set :repo_url, "git@github.com:papayalabs/theturtlefoundation7.git"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'certs'
 append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/aws.yml', 'config/unicorn.rb'
