2868fcda0e16ce4a19bf25d2d729fa48e27691d2
papayalabs <papayalabs@gmail.com>
2024-07-04 16:34:31 -0400

    Updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,6 +81,6 @@ after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:stop'
-    #invoke 'unicorn:start'
+    invoke 'unicorn:start'
   end
 end
